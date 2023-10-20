db731579f1b9048b2e681341ee718cab585c53d3
hsbae <hellospring89@gmail.com>
2023-10-20 21:59:19 +0900

    first commit

---
+++ src/main/java/hello/coretry/CoretryApplication.java
@@ -0,0 +1,13 @@
+package hello.coretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoretryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoretryApplication.class, args);
+	}
+
+}
