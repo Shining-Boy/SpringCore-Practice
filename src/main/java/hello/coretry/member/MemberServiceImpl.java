4adc5f959c1742c79f6ce65c11c6e22e27014041
hsbae <hellospring89@gmail.com>
2023-10-23 09:38:17 +0900

    회원 서비스 인터페이스 및 구현체

---
+++ src/main/java/hello/coretry/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.coretry.member;
+
+public class MemberServiceImpl implements MemberService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);        
+    }
+    
+    
+}
