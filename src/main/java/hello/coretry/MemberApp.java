5acf4d4e9aa196f78b5ddf8b1540dc736cbf6878
hsbae <hellospring89@gmail.com>
2023-10-24 11:06:15 +0900

    회원 서비스 테스트 코드 작성

---
+++ src/main/java/hello/coretry/MemberApp.java
@@ -0,0 +1,17 @@
+package hello.coretry;
+
+import hello.coretry.member.Grade;
+import hello.coretry.member.Member;
+import hello.coretry.member.MemberService;
+import hello.coretry.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
