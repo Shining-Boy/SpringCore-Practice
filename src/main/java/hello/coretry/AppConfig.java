8ab50a5da08f6db9ec1033ae7190ab2bf5ceb7e9
hsbae <hellospring89@gmail.com>
2023-11-19 10:02:52 +0900

    스프링 컨테이너에 빈을 등록해서 가지고 오도록 코드 수정

--- src/main/java/hello/coretry/AppConfig.java
+++ src/main/java/hello/coretry/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.coretry;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.coretry.discount.DiscountPolicy;
 import hello.coretry.discount.FixDiscountPolicy;
 import hello.coretry.discount.RateDiscountPolicy;
@@ -10,11 +13,15 @@ import hello.coretry.member.MemoryMemberRepository;
 import hello.coretry.order.OrderService;
 import hello.coretry.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
             memberRepository(),
@@ -22,10 +29,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/coretry/MemberApp.java
+++ src/main/java/hello/coretry/MemberApp.java
@@ -1,5 +1,8 @@
 package hello.coretry;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.coretry.member.Grade;
 import hello.coretry.member.Member;
 import hello.coretry.member.MemberService;
@@ -8,8 +11,10 @@ import hello.coretry.member.MemberServiceImpl;
 public class MemberApp {
     public static void main(String[] args) {
         //MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new  AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
