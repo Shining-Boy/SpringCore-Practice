3d7ef8871114aa70af2a39e910724b5401c9c379
hsbae <hellospring89@gmail.com>
2024-02-07 11:34:35 +0900

    수동으로 등록한 빈의 이름과  @Component로 등록된 빈이 서로 이름이 동일할 때 오류가 발생함을 확인하는 코드

--- src/main/java/hello/coretry/AutoAppConfig.java
+++ src/main/java/hello/coretry/AutoAppConfig.java
@@ -1,14 +1,22 @@
 package hello.coretry;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan.Filter;
 
+import hello.coretry.member.MemberRepository;
+import hello.coretry.member.MemoryMemberRepository;
 
+@Configuration
 @ComponentScan(
     excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
     
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
