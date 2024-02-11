276cff6662ffb5b7682f4cb1837c4dd42e4ad80a
hsbae <hellospring89@gmail.com>
2024-02-11 22:19:14 +0900

    빈 생명주기 콜백 예제-인터페이스 사용

---
+++ src/test/java/hello/coretry/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.coretry.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void discoonect() {
+        System.out.println("close: " + url);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // TODO Auto-generated method stub
+        discoonect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // TODO Auto-generated method stub
+        connect();
+        call("초기화 연결 메시지");
+    }
+}
