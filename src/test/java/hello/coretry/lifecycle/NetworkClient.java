5c200583f92a74d9615900d68b1def91329727da
hsbae <hellospring89@gmail.com>
2024-02-11 22:30:41 +0900

    빈 생명주기 콜백 예제-애노테이션 이용

--- src/test/java/hello/coretry/lifecycle/NetworkClient.java
+++ src/test/java/hello/coretry/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.coretry.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient /*implements InitializingBean, DisposableBean*/ {
     
     private String url;
@@ -31,12 +34,14 @@ public class NetworkClient /*implements InitializingBean, DisposableBean*/ {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         discoonect();
