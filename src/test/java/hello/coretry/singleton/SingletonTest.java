bdb92db60d268d34bea7c4385046779d1753e734
hsbae <hellospring89@gmail.com>
2024-02-18 09:18:31 +0900

    싱글톤 빈의 생명주기를 확인하는 테스트 코드

--- src/test/java/hello/coretry/singleton/SingletonTest.java
+++ src/test/java/hello/coretry/singleton/SingletonTest.java
@@ -4,14 +4,42 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 import hello.coretry.AppConfig;
 import hello.coretry.member.Member;
 import hello.coretry.member.MemberService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class SingletonTest {
+
+    @Test
+    public void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
     
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
